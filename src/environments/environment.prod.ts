0f5add3b55bc08d35a3bbb9fbe337cbba856cd16
Fatur Gautama S <fatur@access-mobile.com>
2024-04-22 09:28:11 +0700

    feat: change endpoint prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // endpoint: 'https://api-retail.trijuna-pratama.com/api',
-    endpoint: 'https://api-retail.tirta-rudensia.my.id/api',
+    endpoint: 'https://retial-api.trijuna.online/api',
 };
